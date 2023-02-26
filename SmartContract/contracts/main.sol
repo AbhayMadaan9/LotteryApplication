ddfb24adb1fa953122b049f5da4d72ae26f96823
Abhay Madaan <78895271+AbhayMadaan9@users.noreply.github.com>
2023-02-26 19:05:45 +0530

    bug resolved

---
+++ SmartContract/contracts/main.sol
@@ -0,0 +1,46 @@
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract lottery{
+    address public manager;
+    address payable[] public participants;
+
+//In the starting of contract we inform that manager has the control of contract by giving the address of contract.
+    constructor()  
+    {
+        manager = msg.sender;  //Every smart contract invocation has a caller address. Each EVM (Ethereum Virtual Machine that executes the code) knows which account carries out each action. In Solidity, you can access the calling account by referencing msg.sender So when you call a function of solidity contract, your contract already gets the information of your account, so your account is the msg.sender
+    }
+
+    receive() external payable //this is special inbuilt func. used to get ether from participants in contract mostly used with external keyword and it is always payable and it has no parameters
+    {
+         //this is like if statemenet below code is executed if require statement is true.
+        require(msg.value == 2 ether);
+        participants.push(payable(msg.sender));
+    }
+    function getbalance() public view returns(uint) {
+        require(msg.sender == manager);
+        return address(this).balance;
+    }
+    //https://ethereum.stackexchange.com/questions/11572/how-does-the-keccak256-hash-function-work/45585,
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.timestamp, participants.length))); //this hash function is used to generate random from participants it is converted to uint. It is not usually used
+    }
+    //we use the logic that we take modulo of random generated value with lenth of participants since on taking the moduo the number generated will lie from 0-lengthOfParticipants.
+    function selectWinner() public {
+        require(msg.sender == manager);
+        require(participants.length >= 3);
+
+        uint rand = random();
+        uint len = participants.length;
+        address payable winner;
+        uint index = rand%len;
+        winner = participants[index];
+        winner.transfer(getbalance());
+        participants = new address payable[](0); //reseting the participants array
+    }
+
+    function allParticipants() view public returns (address payable[] memory) {
+        return  participants;
+    }
+}
---
+++ SmartContract/migrations/1_deploy_contracts.js
@@ -0,0 +1,6 @@
+const Lottery = artifacts.require("./main.sol");
+
+module.exports = function (deployer) {
+  deployer.deploy(Lottery);
+};
+//  0x23864cC870eC43757731D5E0606256159b2Cd4E1 -- alchamey Contract (old)
